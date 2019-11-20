6d252e0f212fec627047d2df00cfa36d0d21ae68
gremblin <e2m32@users.noreply.github.com>
2019-11-20 09:36:56 -0500

    Default changes
    - New default settings based on feedback from github community
      - restore: true
      - restore_grace_period: 0
    - Fixed setting of defaults in CONFIG_SCHEMA
      - default duration now set properly to 0 sec, this prevents
         the previous error when starting a timer with a None duration.
    - Removed now unnecessary code for setting defaults in __init__

--- custom_components/timer/__init__.py
+++ custom_components/timer/__init__.py
@@ -18,8 +18,8 @@ DOMAIN = "timer"
 ENTITY_ID_FORMAT = DOMAIN + ".{}"
 
 DEFAULT_DURATION = 0
-DEFAULT_RESTORE = False
-DEFAULT_RESTORE_GRACE_PERIOD = timedelta(minutes=15)
+DEFAULT_RESTORE = True
+DEFAULT_RESTORE_GRACE_PERIOD = timedelta()
 ATTR_DURATION = "duration"
 ATTR_REMAINING = "remaining"
 ATTR_RESTORE = "restore"
@@ -58,13 +58,13 @@ CONFIG_SCHEMA = vol.Schema(
                     vol.Optional(CONF_NAME): cv.string,
                     vol.Optional(CONF_ICON): cv.icon,
                     vol.Optional(
-                        CONF_DURATION, timedelta(DEFAULT_DURATION)
+                        CONF_DURATION, default=timedelta(DEFAULT_DURATION)
                     ): cv.time_period,
                     vol.Optional(
-                        CONF_RESTORE, DEFAULT_RESTORE
+                        CONF_RESTORE, default=DEFAULT_RESTORE
                     ): cv.boolean,
                     vol.Optional(
-                        CONF_RESTORE_GRACE_PERIOD, DEFAULT_RESTORE_GRACE_PERIOD
+                        CONF_RESTORE_GRACE_PERIOD, default=DEFAULT_RESTORE_GRACE_PERIOD
                     ): cv.time_period,
                 },
                 None,
@@ -123,12 +123,9 @@ class Timer(RestoreEntity):
         self._state = STATUS_IDLE
         self._duration = duration
         self._remaining = self._duration
-        self._restore = restore if restore is not None \
-                        else DEFAULT_RESTORE
+        self._restore = restore 
         if self._restore:
-            self._restore_grace_period = restore_grace_period \
-                                    if restore_grace_period is not None \
-                                    else DEFAULT_RESTORE_GRACE_PERIOD
+            self._restore_grace_period = restore_grace_period
         else:
             self._restore_grace_period = None
         
